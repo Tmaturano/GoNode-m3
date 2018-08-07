9281758b22d04461f963b17abfc7fa90c922edde
Thiago Maturana <thiagomatu@hotmail.com>
2018-08-07 20:23:39 -0300

    included the body-parser package to receive json in requests

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const app = require('express')();
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const bodyParser = require('body-parser');
 
 const dbConfig = require('./config/database');
 
 mongoose.connect(dbConfig.url);
 requireDir(dbConfig.modelsPath);
 
-const User = mongoose.model('User');
-User.create({
-  name: 'Thiago',
-  userName: 'tmaturano',
-  email: 'thiagomatu@gmail.com',
-  password: '1234',
-}, () => {
-  console.log('OK');
+app.use(bodyParser.json());
+
+app.post('/create', async (req, res) => {
+  const User = mongoose.model('User');
+  await User.create(req.body);
+
+  return res.send();
 });
 
 app.listen(3000);
