067b23c279d83311193d740902e2c76b584146e5
Thiago Maturana <thiagomatu@hotmail.com>
2018-08-13 19:34:34 -0300

    included the Sentry configuration for error management. Included dotenv package to define environment variables

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const app = require('express')();
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const bodyParser = require('body-parser');
+const Raven = require('./app/services/sentry');
 
 const dbConfig = require('./config/database');
 
+require('dotenv').config();
 mongoose.connect(dbConfig.url);
 requireDir(dbConfig.modelsPath);
 
 app.use(bodyParser.json());
+
+app.use(Raven.requestHandler());
+
 app.use('/api', require('./app/routes'));
+
+// Detect any errors in the routes and send to sentry the details
+app.use(Raven.errorHandler());
 
 app.listen(3000);
