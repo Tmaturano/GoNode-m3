5784f6e20f884722532b85313fd99c7e1985724e
Thiago Maturana <thiagomatu@hotmail.com>
2018-08-15 20:08:14 -0300

    included a new method in userController to get informations from the logged user

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -3,7 +3,25 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('User');
 
+const Tweet = mongoose.model('Tweet');
+
 module.exports = {
+  async information(req, res, next) {
+    try {
+      const user = await User.findById(req.userId);
+      const tweetCount = await Tweet.find({ user: user.id }).count();
+
+      return res.json({
+        user,
+        tweetCount,
+        followersCount: user.followers.length,
+        following: user.following.length,
+      });
+    } catch (err) {
+      return next(err);
+    }
+  },
+
   async update(req, res, next) {
     try {
       const id = req.userId;
--- app/routes.js
+++ app/routes.js
@@ -31,6 +31,7 @@ routes.delete('/tweets/:id', controllers.tweetController.destroy);
  * Users
  */
 routes.put('/users', controllers.userController.update);
+routes.get('/users/me', controllers.userController.information);
 
 /**
  * Follow
